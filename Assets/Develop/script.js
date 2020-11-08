2cfd2e78326920ad62da19103a935ac4623158de
lana <lanabkim@gmail.com>
2020-11-07 22:15:18 -0800

    add html and date display

--- Assets/Develop/script.js
+++ Assets/Develop/script.js
@@ -1,6 +1,22 @@
 
 // Get the current hour of the day using moment.js
 
+// setting moment() as var so we can call it once and store and reuse them in different places. 
+var currentMoment= moment(); 
+
+
+// get today's date and display  "day of week, month date year, time am/pm"
+var now = currentMoment.format("dddd, MMMM Do YYYY, h:mm:ss a"); 
+
+// display currenttime in "currentDay" in html
+
+var dateDisplay =  $("#currentDay") 
+dateDisplay.text(now);
+
+
+var currentHour = parseInt(currentMoment.format("H")); 
+var currentHourtwo = currentMoment.hour(); 
+console.log( currentHour, currentHourtwo ); 
 // For  Loop pver the hours of the day from 9am to 5pm 
 //   i = 9 to i <= 17 
 
@@ -24,7 +40,9 @@
 
 // to my save button clicks 
 
-// 
+// When the user clicks a save button 
 
+// than i need to save the associated text for the associated user 
 
+event.target
 
