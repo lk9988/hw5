aea3bed5154099f2519ba5cab4d29f1858c6e13b
lana <lanabkim@gmail.com>
2020-11-07 19:43:17 -0800

    initial files

---
+++ Assets/Develop/script.js
@@ -0,0 +1,30 @@
+
+// Get the current hour of the day using moment.js
+
+// For  Loop pver the hours of the day from 9am to 5pm 
+//   i = 9 to i <= 17 
+
+// var i = 9
+
+//  Current checking 9am < current hour 
+
+// 9am is in the past 
+
+// < div id = "hour-9" > 로 만들려면 
+
+// Element ID =  "hour- " + i 
+
+// Select Matching element - $( Element ID )
+
+// saving / loading data 
+
+// saving it to localstorage 
+
+// add a click event on a parent element that can listen 
+
+// to my save button clicks 
+
+// 
+
+
+
